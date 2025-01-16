1740f95bb2a3066d440ed73e8663c98d97ec6f8e
shubmehetre <ghostfr3k@protonmail.com>
2025-01-16 20:43:45 +0530

    routine

--- config.h
+++ config.h
@@ -3,9 +3,10 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	/* {"⌨", "sb-kbselect", 0, 30}, */
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
+	{"",	"sb-schedule",	1,	12},
 	{"",	"sb-tasks",	10,	26},
 	{"",	"sb-music",	0,	11},
-	{"",	"sb-pacpackages",	0,	8},
+	{"",	"sb-pacpackages",	10,	8},
 	/*{"",	"sb-news",		0,	6},*/
 	/* {"",	"sb-price lbc \"LBRY Token\" 📚",			9000,	22}, */
 	/* {"",	"sb-price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
